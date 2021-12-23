e28ac8478efeadb933e09858b855d29fdf6a0776
Kamome <makinoharashoko@outlook.com>
2021-12-23 20:12:55 +0800

    create

---
+++ index.js
@@ -0,0 +1,43 @@
+class flyVar {
+    flyStorage = {};
+    flyEffect = [];
+    flySingle = {};
+    set(k, v) {
+        this.flyStorage[k] = v;
+        this.runAll();
+        this.runSingle(k);
+    }
+    effect(callback) {
+        if (typeof callback === 'function')
+            this.flyEffect.push(callback)
+    }
+    get(k) {
+        if (this.flyStorage.hasOwnProperty(k))
+            return this.flyStorage[k];
+    }
+    connect(k, callback) {
+        this.flySingle[k] = new Object();
+        this.flySingle[k].callback = callback;
+    }
+    runAll() {
+        //run global
+        for (const callback of this.flyEffect) {
+            callback();
+        }
+    }
+    runSingle(k) {
+        //run connected function
+        if (this.hasOwnProperty(k) &&
+            this.flySingle[k].hasOwnProperty('callback') &&
+            typeof this.flySingle[k]['callback'] === 'function')
+            this.flySingle[k].callback();
+    }
+}
+
+const store = new flyVar;
+store.connect('value2', () => { console.log(store.get('value')); })
+for (let i = 0; i < 10; i++) {
+    store.set('value', i);
+}
+
+module.exports = flyVar;
